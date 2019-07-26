03bfdd48c365f3b12e5ad6deafb6fa5ff3eac9e6
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-07-26 14:06:38 +0300

    update

--- resources/views/components/users/user-card.blade.php
+++ resources/views/components/users/user-card.blade.php
@@ -1,9 +1,25 @@
 <div class="user-card">
-    <div class="user-card__photo user-card__dot-status">
-        <img src="uploads/images/default.png" alt="">
+    <div class="user-card__photo">
+        <a href="{{ route('user.profile', $user->route ?? $user->username) }}" title="{{ $user->username }}">
+            <img src="uploads/images/default.png" alt="{{ $user->username }}">
+            @if( $user->is_online() )
+                <span class="user-card__dot-status user-card__dot-status--online"></span>
+            @else
+                <span class="user-card__dot-status"></span>
+            @endif
+        </a>
     </div>
     <div class="user-card__information">
-        <div class="user-card__name">{{ $user->username }}</div>
-        <div class="user-card__status">онлайн 29 минут назад</div>
+        <a class="user-card__name" href="{{ route('user.profile', $user->route ?? $user->username) }}" title="{{ $user->username }}">
+            {{ $user->username }}
+        </a>
+
+        @if($user->is_online())
+            <div class="user-card__status">онлайн</div>
+        @else
+            <div class="user-card__status">
+                {{ UserHelper::getOnlineTime($user->profile->gender, $user->profile->offline_at->diffForHumans()) }}
+            </div>
+        @endif
     </div>
 </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.default')
 
 @section('content')
-    USERS
+    <div class="content-header">
+        <div class="content-header__title">
+            <h1 class="content-header__title-text">Пользователи</h1>
+        </div>
+        <ul class="content-header__menu">
+            <li class="content-header__menu-item"><a href="#" class="content-header__menu-link content-header__menu-link--active">Рейтинг</a></li>
+            <li class="content-header__menu-item"><a href="#" class="content-header__menu-link">Последние</a></li>
+            <li class="content-header__menu-item"><a href="#" class="content-header__menu-link">В сети</a></li>
+        </ul>
+    </div>
+
     @forelse ($users as $user)
         @include('components.users.user-card', [ 'user' => $user ])
     @empty
