897c8f986e092400511536c26dc255c11c2fa54e
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-09-13 14:05:17 +0300

    update

--- app/Http/Controllers/Modules/TNParseController.php
+++ app/Http/Controllers/Modules/TNParseController.php
@@ -9,17 +9,23 @@
 namespace App\Http\Controllers\Modules;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class TNParseController extends Controller {
 
 
-    public function index()
-    {
-
-
-
+    public function index(){
         return view('modules.TNParse.index');
     }
+
+
+    public function upload(Request $request) {
+
+        if($request->ajax() && $request->method() == 'POST')
+            return json_decode('done');
+        return abort(404);
+    }
+
 
 
 }
--- resources/views/modules/TNParse/index.blade.php
+++ resources/views/modules/TNParse/index.blade.php
@@ -18,13 +18,18 @@
 
 
     <div class="mt-5 p-3 bg-light border border-primary rounded">
-        <form id="import" method="POST" action="{{ route('modules.TNParse.upload') }}">
+        <form id="import-form" method="POST" action="{{ route('modules.TNParse.upload') }}" enctype="multipart/form-data">
             @csrf
+
+            <div class="progress" style="height: 30px;">
+                <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <br>
             <div class="form-group">
                 <label for="input-file">Выберите файл (CSV)</label>
                 <input type="file" name="file" class="form-control-file" id="input-file">
             </div>
-            <button type="button" class="btn btn-primary disabled">
+            <button type="submit" class="btn btn-primary disabled" id="button-submit">
                 Импорт
             </button>
         </form>
@@ -35,9 +40,64 @@
 
 @push('scripts')
     <script>
-        $(document).ready(function (e) {
+        $(function () {
 
-        })
+            $('form#import-form').on('change', function () {
+
+                let inputFile = $('input[type=file]')[0].files[0];
+                let btnSubmit = $('#button-submit');
+
+                if (!inputFile) {
+                    btnSubmit.addClass('disabled');
+                } else {
+                    btnSubmit.removeClass('disabled');
+                }
+            });
+
+
+            $('form#import-form').submit(function (e) {
+                e.preventDefault();
+
+                if (!$('#button-submit').hasClass('disabled')) {
+
+                    e.stopImmediatePropagation();
+                    let formData = new FormData($(this)[0]);
+                    let file = $('input[type=file]')[0].files[0];
+                    formData.append('upload_file', file);
+
+                    $('.progress').show();
+
+                    $.ajax({
+                        xhr: function () {
+                            let xhr = new window.XMLHttpRequest();
+                            xhr.upload.addEventListener("progress", function (evt) {
+                                if (evt.lengthComputable) {
+                                    let percentComplete = evt.loaded / evt.total;
+                                    percentComplete = parseInt(percentComplete * 100);
+                                    $('.progress-bar').css('width', percentComplete + "%");
+                                    $('.progress-bar').html(percentComplete + "%");
+                                    if (percentComplete === 100) {
+
+                                    }
+                                }
+                            }, false);
+                            return xhr;
+                        },
+                        type: 'POST',
+                        url: '{{ route('modules.TNParse.upload') }}',
+                        data: formData,
+                        async: true,
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        success: function (returndata) {
+                            console.log(returndata);
+                            $('#result').html(returndata);
+                        }
+                    });
+                    return false;
+                }
+            });
+        });
     </script>
-
 @endpush
