84bc2dbb73f5f6de04786f346a5e6e660280692e
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-09-16 18:00:55 +0300

    update

--- app/Http/Controllers/Modules/TNParseController.php
+++ app/Http/Controllers/Modules/TNParseController.php
@@ -32,26 +32,25 @@ class TNParseController extends Controller {
                 while (($data = fgetcsv($handle, '', '|')) !== FALSE) {
 
                     if(isset($data[1]))
-                        $data[1] =  iconv('cp866', 'UTF-8', trim($data[1]));
+                        $data[1] = iconv('cp866', 'UTF-8', trim($data[1]));
                     if(isset($data[2]))
-                        $data[2] =  iconv('cp866', 'UTF-8', trim($data[2]));
+                        $data[2] = iconv('cp866', 'UTF-8', trim($data[2]));
                     if(isset($data[3]))
-                        $data[3] =  iconv('cp866', 'UTF-8', trim($data[3]));
+                        $data[3] = iconv('cp866', 'UTF-8', trim($data[3]));
                     if(isset($data[4]))
-                        $data[4] =  iconv('cp866', 'UTF-8', trim($data[4]));
+                        $data[4] = iconv('cp866', 'UTF-8', trim($data[4]));
                     if(isset($data[5]))
-                        $data[5] =  iconv('cp866', 'UTF-8', trim($data[5]));
+                        $data[5] = iconv('cp866', 'UTF-8', trim($data[5]));
 
-                   // print_r($data);
 
-                   // $num = count($data);
+                    // $num = count($data);
                     //echo "<p> " . $num . " .полей в строке " . $row ." : <br /></p>\n";
                     echo "<strong style='color: #83a6f7;'>" . $data[0]  ."</strong>   <strong>". $data[1] . "</strong>";
                     echo "<p style='color: #a3a3a5;border-bottom: 1px Dashed #e6e6e6;padding: 0 0 1rem;'>" . $data[2] . "</p>";
 
-//                    for ($c=0; $c < $num; $c++) {
-//                        echo $data[$c] . "<br />\n";
-//                    }
+                    //for ($c=0; $c < $num; $c++) {
+                    //    echo $data[$c] . "<br />\n";
+                    //}
 
 
 
@@ -64,7 +63,7 @@ class TNParseController extends Controller {
 
         }
 
-        return abort(404);
+        //return abort(404);
     }
 
 
