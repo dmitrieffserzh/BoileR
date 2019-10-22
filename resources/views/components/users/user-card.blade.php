a1dbc0f8b01ab4f9d4171398aabb5604a36ef197
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2019-10-22 12:57:50 +0300

    update

--- resources/views/components/users/user-card.blade.php
+++ resources/views/components/users/user-card.blade.php
@@ -2,24 +2,22 @@
     <div class="user-card__photo">
         <a href="{{ route('user.profile', $user->route ?? $user->username) }}" title="{{ $user->username }}">
             <img src="{{ UserHelper::get_avatar($user->profile->avatar) }}" alt="{{ $user->username }}">
-            @if( $user->is_online() )
-                <span class="user-card__dot-status user-card__dot-status--online"></span>
-            @else
-                <span class="user-card__dot-status"></span>
-            @endif
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"
+                 fill="{{ $user->is_online() ? '#00d006' : '#bbd0ea' }}" stroke="#fff" stroke-width="2"
+                 class="user-card__dot-status">
+                <circle cx="6" cy="6" r="5"/>
+            </svg>
         </a>
     </div>
-    <div class="user-card__information">
-        <a class="user-card__name" href="{{ route('user.profile', $user->route ?? $user->username) }}" title="{{ $user->username }}">
+    <div class="user-card__info">
+        <a class="user-card__name" href="{{ route('user.profile', $user->route ?? $user->username) }}"
+           title="{{ $user->username }}">
             {{ $user->username }}
         </a>
 
-        @if($user->is_online())
-            <div class="user-card__status">онлайн</div>
-        @else
-            <div class="user-card__status">
-                {{ UserHelper::getOnlineTime($user->profile->gender, $user->profile->offline_at->diffForHumans()) }}
-            </div>
-        @endif
+        <div class="user-card__status">
+            {{ $user->is_online() ? 'онлайн' : UserHelper::getOnlineTime($user->profile->gender, $user->profile->offline_at->diffForHumans()) }}
+        </div>
+
     </div>
 </div>
